cbc71b614466bf3cc4602ac1909ae3116ac353ce
thusharathiwanka <thusharathiwank123@gmail.com>
2020-06-23 03:36:22 +0530

    footer fixed

--- js/menu.js
+++ js/menu.js
@@ -3,13 +3,13 @@ const nav = document.querySelector(".nav-links ul");
 const menuClose = document.querySelector("#menu-close");
 
 menuOpen.addEventListener("click", () => {
-  nav.classList.toggle("nav-open");
   menuOpen.style.display = "none";
+  nav.classList.toggle("nav-open");
   menuClose.style.display = "block";
 });
 
 menuClose.addEventListener("click", () => {
   nav.classList.toggle("nav-open");
-  menuClose.style.display = "none";
   menuOpen.style.display = "block";
+  menuClose.style.display = "none";
 });
