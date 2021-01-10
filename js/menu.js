db83d771170ef79a8577a562ed6c992d17daa61f
thusharathiwanka <thusharathiwanka123@gmail.com>
2021-01-10 10:02:11 +0530

    UPDATE: mobile navigation changed

--- js/menu.js
+++ js/menu.js
@@ -3,13 +3,13 @@ const nav = document.querySelector(".nav-links ul");
 const menuClose = document.querySelector("#menu-close");
 
 menuOpen.addEventListener("click", () => {
-  menuOpen.style.display = "none";
+  menuOpen.style.visibility = "hidden";
   nav.classList.toggle("nav-open");
   menuClose.style.display = "block";
 });
 
 menuClose.addEventListener("click", () => {
   nav.classList.toggle("nav-open");
-  menuOpen.style.display = "block";
+  menuOpen.style.visibility = "visible";
   menuClose.style.display = "none";
 });
